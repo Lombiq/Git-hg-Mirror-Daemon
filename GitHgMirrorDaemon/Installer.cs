b7d7552ef1e02852e3891098ed9d3127b09ae85f
Zoltán Lehóczky <lehoczky_zoltan@pyrocenter.hu>
2013-08-25 02:43:54 +0200

    Adding self-installer and moving folders around

---
+++ GitHgMirrorDaemon/Installer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitHgMirrorDaemon
+{
+    [RunInstaller(true)]
+    public class Installer : System.Configuration.Install.Installer
+    {
+        public Installer()
+        {
+            var process = new ServiceProcessInstaller();
+            process.Account = ServiceAccount.LocalSystem;
+            var serviceAdmin = new ServiceInstaller();
+            serviceAdmin.StartType = ServiceStartMode.Manual;
+            serviceAdmin.ServiceName = "GitHgMirrorService";
+            serviceAdmin.DisplayName = "GitHgMirrorService";
+            Installers.Add(process);
+            Installers.Add(serviceAdmin);
+        }
+    }
+}
--- GitHgMirrorDaemon/Program.cs
+++ GitHgMirrorDaemon/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration.Install;
 using System.Linq;
+using System.Reflection;
 using System.ServiceProcess;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,17 +11,62 @@ namespace GitHgMirrorDaemon
 {
     static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
         static void Main()
         {
-            ServiceBase[] ServicesToRun;
-            ServicesToRun = new ServiceBase[] 
-            { 
-                new GitHgMirrorService() 
-            };
-            ServiceBase.Run(ServicesToRun);
+            // Below code taken mostly from http://www.codeproject.com/Articles/27112/Installing-NET-Windows-Services-the-easiest-way
+            var service = ServiceController.GetServices().Where(controller => controller.ServiceName == "GitHgMirrorService").SingleOrDefault();
+
+            // Service not installed
+            if (service == null)
+            {
+                SelfInstaller.InstallMe();
+            }
+            // Service is not starting
+            else if (service.Status != ServiceControllerStatus.StartPending)
+            {
+                SelfInstaller.UninstallMe();
+            }
+            // Started from the SCM
+            else
+            {
+                System.ServiceProcess.ServiceBase[] servicestorun;
+                servicestorun = new System.ServiceProcess.ServiceBase[] { new GitHgMirrorService() };
+                ServiceBase.Run(servicestorun);
+            }
+        }
+    }
+
+    static class SelfInstaller
+    {
+        private static readonly string _exePath = Assembly.GetExecutingAssembly().Location;
+
+
+        public static bool InstallMe()
+        {
+            try
+            {
+                ManagedInstallerClass.InstallHelper(new string[] { _exePath });
+            }
+            catch
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public static bool UninstallMe()
+        {
+            try
+            {
+                ManagedInstallerClass.InstallHelper(new string[] { "/u", _exePath });
+            }
+            catch
+            {
+                return false;
+            }
+
+            return true;
         }
     }
 }
