534c4894a67724ea0d79d88fe93d9cdf8fa37ce8
Zoltán Lehóczky <lehoczky_zoltan@pyrocenter.hu>
2013-08-27 20:11:52 +0200

    Completing daemon
    --HG--
    branch : dev

--- GitHgMirror.Daemon/GitHgMirrorService.cs
+++ GitHgMirror.Daemon/GitHgMirrorService.cs
@@ -8,11 +8,16 @@ using System.ServiceProcess;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using GitHgMirror.Runner;
 
 namespace GitHgMirror.Daemon
 {
     public partial class GitHgMirrorService : ServiceBase
     {
+        private MirrorRunner _runner;
+        private ManualResetEvent _waitHandle = new ManualResetEvent(false);
+
+
         public GitHgMirrorService()
         {
             InitializeComponent();
@@ -28,18 +33,36 @@ namespace GitHgMirror.Daemon
             timer.Enabled = true;
         }
 
+        protected override void OnStop()
+        {
+            serviceEventLog.WriteEntry("GitHgMirrorDaemon stopped. Stopping mirroring.");
+
+            _runner.Stop();
+
+            serviceEventLog.WriteEntry("Mirroring stopped.");
+        }
+
+
         void timer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
         {
             ((System.Timers.Timer)sender).Enabled = false;
-            serviceEventLog.WriteEntry("valami");
-            Thread.Sleep(60000);
-            serviceEventLog.WriteEntry("valami2");
-            Thread.Sleep(99999999);
-        }
 
-        protected override void OnStop()
-        {
-            serviceEventLog.WriteEntry("GitHgMirrorDaemon stopped.");
+            serviceEventLog.WriteEntry("Starting mirroring.");
+
+            var settings = new Settings
+            {
+                ApiEndpointUrl = new Uri("http://githgmirror.com/api/GitHgMirror.Common/Mirrorings"),
+                ApiPassword = "Fsdfp342LE8%!",
+                RepositoriesDirectoryPath = @"C:\GitHgMirror\Repositories"
+            };
+
+            _runner = new MirrorRunner(settings, serviceEventLog);
+
+            _runner.Start();
+
+            serviceEventLog.WriteEntry("Mirroring started.");
+
+            _waitHandle.WaitOne();
         }
     }
 }
