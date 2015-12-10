850185bdecbd828020090fcb2352086375df6680
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2015-12-11 00:43:35 +0100

    Two-way mirroring now happens as a hg to git and then a separate git to hg step
    --HG--
    branch : dev

--- GitHgMirror.CommonTypes/MirroringConfiguration.cs
+++ GitHgMirror.CommonTypes/MirroringConfiguration.cs
@@ -13,5 +13,19 @@ namespace GitHgMirror.CommonTypes
         public Uri GitCloneUri { get; set; }
         public bool GitUrlIsHgUrl { get; set; }
         public MirroringDirection Direction { get; set; }
+
+
+        public MirroringConfiguration(MirroringConfiguration previous) : this()
+        {
+            Id = previous.Id;
+            HgCloneUri = previous.HgCloneUri;
+            GitCloneUri = previous.GitCloneUri;
+            GitUrlIsHgUrl = previous.GitUrlIsHgUrl;
+            Direction = previous.Direction;
+        }
+
+        public MirroringConfiguration()
+        {
+        }
     }
 }
