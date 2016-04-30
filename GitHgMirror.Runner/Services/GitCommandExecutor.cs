033535ff841f45cb79fe3d084e684c69ce49dd5a
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2016-04-30 02:35:16 +0200

    Fixing that GitHub PRs were merged on git push
    --HG--
    branch : dev

--- GitHgMirror.Runner/Services/GitCommandExecutor.cs
+++ GitHgMirror.Runner/Services/GitCommandExecutor.cs
@@ -203,7 +203,13 @@ namespace GitHgMirror.Runner.Services
                         "Starting to fetch from git repo: " + gitCloneUri + " (" + cloneDirectoryPath + ").",
                         EventLogEntryType.Information);
 
-                    repository.Network.Fetch(repository.Network.Remotes["origin"], new[] { "+refs/*:refs/*" });
+                    // We can't just use the +refs/*:refs/* refspec since on GitHub PRs have their own specials refs as
+                    // refs/pull/[ID]/head and refs/pull/[ID]/merge refs. Pushing a latter ref merges the PR, what of
+                    // course we don't want. So we need to filter just the interesting refs.
+                    // Also we really shouldn't fetch and push other namespaces like meta/config either, see:
+                    // https://groups.google.com/forum/#!topic/repo-discuss/zpqpPpHAwSM
+                    repository.Network.Fetch(repository.Network.Remotes["origin"], new[] { "+refs/heads/*:refs/heads/*" });
+                    repository.Network.Fetch(repository.Network.Remotes["origin"], new[] { "+refs/tags/*:refs/tags/*" });
 
                     _eventLog.WriteEntry(
                         "Finished fetching from git repo: " + gitCloneUri + " (" + cloneDirectoryPath + ").",
