1034fb23b1229ae4e8dfd941459e18ce2876ef21
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2017-02-11 19:05:11 +0100

    Increasing the maximum size of the event log to accommodate the increased amount of log entries per time frame due to the higher degree of parallelism
    --HG--
    branch : dev

--- GitHgMirror.Tester/Program.cs
+++ GitHgMirror.Tester/Program.cs
@@ -23,6 +23,7 @@ namespace GitHgMirror.Tester
 
             using (var eventLog = new EventLog("Git-hg Mirror Daemon", ".", "GitHgMirror.Tester"))
             {
+                eventLog.MaximumKilobytes = 4194240; // 4GB
                 eventLog.EnableRaisingEvents = true;
 
                 eventLog.EntryWritten += (sender, e) =>
