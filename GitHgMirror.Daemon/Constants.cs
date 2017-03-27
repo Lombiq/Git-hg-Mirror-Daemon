de0d9e3054b52b6fedfa8f02754d1bc39198bb81
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2017-03-27 14:46:27 +0200

    Making all Daemon settings configurable from the App.config
    --HG--
    branch : dev

--- GitHgMirror.Daemon/Constants.cs
+++ GitHgMirror.Daemon/Constants.cs
@@ -9,8 +9,29 @@ namespace GitHgMirror.Daemon
     public static class Constants
     {
         /// <summary>
+        /// Configuation key for the API endpoint URL used to access the GitHgMirror API when fetching mirroring 
+        /// configurations.
+        /// </summary>
+        public static string ApiEndpointUrl = "GitHgMirror.Daemon.ApiEndpointUrl";
+
+        /// <summary>
         /// Configuration key for password used to access the GitHgMirror API when fetching mirroring configurations.
         /// </summary>
         public static string ApiPasswordKey = "GitHgMirror.Daemon.ApiPassword";
+
+        /// <summary>
+        /// Configuration key for the path pointing to the directory where mirrored repositories are stored.
+        /// </summary>
+        public static string RepositoriesDirectoryPath = "GitHgMirror.Daemon.RepositoriesDirectoryPath";
+
+        /// <summary>
+        /// Configuration key for the maximal degree of parallelism mirrorings are executed.
+        /// </summary>
+        public static string MaxDegreeOfParallelism = "GitHgMirror.Daemon.MaxDegreeOfParallelism";
+
+        /// <summary>
+        /// Configuration key for how many mirrorings are executed in the same task after each other.
+        /// </summary>
+        public static string BatchSize = "GitHgMirror.Daemon.BatchSize";
     }
 }
