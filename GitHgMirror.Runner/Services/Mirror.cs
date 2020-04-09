3f5744070bf519e0e62617b88719fafc51f128ae
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2020-04-09 19:52:38 +0200

    Formatting

--- GitHgMirror.Runner/Services/Mirror.cs
+++ GitHgMirror.Runner/Services/Mirror.cs
@@ -1,12 +1,11 @@
 ﻿using GitHgMirror.CommonTypes;
 using GitHgMirror.Runner.Helpers;
+using LibGit2Sharp;
 using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Threading;
-using LibGit2Sharp;
 
 namespace GitHgMirror.Runner.Services
 {
@@ -25,8 +24,8 @@ namespace GitHgMirror.Runner.Services
 
 
         public void MirrorRepositories(
-            MirroringConfiguration configuration, 
-            MirroringSettings settings, 
+            MirroringConfiguration configuration,
+            MirroringSettings settings,
             CancellationToken cancellationToken)
         {
             var descriptor = GetMirroringDescriptor(configuration);
@@ -45,7 +44,7 @@ namespace GitHgMirror.Runner.Services
             {
                 if (File.Exists(repositoryLockFilePath))
                 {
-                    var logEntryStart = 
+                    var logEntryStart =
                         "An existing lock was found for the mirroring configuration " + loggedDescriptor + ". ";
                     var lastUpdatedTimeUtc = RepositoryInfoFileHelper.GetLastUpdatedDateTimeUtc(cloneDirectoryPath);
 
@@ -55,7 +54,7 @@ namespace GitHgMirror.Runner.Services
                             logEntryStart +
                             "This can mean that the number of configurations was reduced and thus while a mirroring was running a new process for the same repositories was started. We'll let the initial process finish.");
 
-                        return; 
+                        return;
                     }
                     else
                     {
@@ -368,7 +367,7 @@ namespace GitHgMirror.Runner.Services
                             throw new MirroringException(exceptionMessage, ex, directoryDeleteException);
                         }
                     }
-                    catch (Exception forcedCleanUpException) 
+                    catch (Exception forcedCleanUpException)
                     when (!forcedCleanUpException.IsFatalOrCancellation() && !(forcedCleanUpException is MirroringException))
                     {
                         throw new MirroringException(
